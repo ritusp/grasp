e58271d647ab5d782e6108b4972832dee2f20340
ritus <ritusenpandey@gmail.com>
2018-07-20 19:21:16 -0500

    initial setup

---
+++ src/main/java/com/grasp/controller/MainController.java
@@ -0,0 +1,55 @@
+package com.grasp.controller;
+
+import com.grasp.util.Calculation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+
+
+public class MainController {
+
+    @RequestMapping(value = "")
+    public String index(Model model){
+
+        model.addAttribute("title","Hello World");
+
+        return "index";
+    }
+
+    @RequestMapping(value = "practicetest", method = RequestMethod.GET)
+    public String displayTestForm(Model model) {
+        model.addAttribute("title", "Test");
+        return "practicetest";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+
+        Calculation calculation = new Calculation();
+
+        String operation;
+
+        List<String>  results = calculation.results(operation);
+
+        if(results == null ){
+            //TODO:
+        }
+
+
+        model.addAttribute("title", "Addition");
+
+        return "calculation";
+    }
+
+
+    @RequestMapping(value = "result", method = RequestMethod.GET)
+    public String displayResult(Model model){
+        model.addAttribute("title", "Result");
+        return "result";
+    }
+}
---
+++ src/main/java/com/grasp/util/Calculation.java
@@ -0,0 +1,40 @@
+package com.grasp.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculation {
+
+
+    public Calculation() {
+    }
+
+    public List<String> results(String opertion){
+
+        List<String> results = new ArrayList<>();
+
+        if(opertion.equalsIgnoreCase("ADD")){
+
+
+
+            return results;
+        }
+
+        else if(opertion.equalsIgnoreCase("SUBSTRACT")){
+
+
+
+            return results;
+        }
+
+        else if(opertion.equalsIgnoreCase("MULTIPLY")){
+
+
+
+            return results;
+        }
+
+        return null;
+    }
+
+}
